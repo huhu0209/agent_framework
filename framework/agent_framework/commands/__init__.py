14f564f510517ad9bfe9ebd0546bbfcf4dde13a6
xiaohu <xiaohu.zhang@ruhlamat.com.cn>
2026-05-26 21:56:05 +0800

    feat(commands): 填充 commands 模块公开导出

--- framework/agent_framework/commands/__init__.py
+++ framework/agent_framework/commands/__init__.py
@@ -1,1 +1,11 @@
 """Slash Commands 系统。"""
+
+from agent_framework.commands.router import CommandRouter
+from agent_framework.commands.types import CommandSource, ResolvedCommand, SlashCommand
+
+__all__ = [
+    "CommandRouter",
+    "CommandSource",
+    "ResolvedCommand",
+    "SlashCommand",
+]
